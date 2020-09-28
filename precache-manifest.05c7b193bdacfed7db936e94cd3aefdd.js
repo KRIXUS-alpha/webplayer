cfe44f9cd6c0c75971820db3f2088cb85ca588b2
KRIXUS-alpha <kartikrockson1@gmail.com>
2020-09-28 19:00:17 +0530

    Updates

---
+++ precache-manifest.05c7b193bdacfed7db936e94cd3aefdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20fff06e12ee9c5fa54d87c77dc01209",
+    "url": "/webplayer/index.html"
+  },
+  {
+    "revision": "f82e0d64e7235a185eb9",
+    "url": "/webplayer/static/css/main.f5dc80e8.chunk.css"
+  },
+  {
+    "revision": "51e42bac03a293d5574c",
+    "url": "/webplayer/static/js/2.840434fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webplayer/static/js/2.840434fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82e0d64e7235a185eb9",
+    "url": "/webplayer/static/js/main.b61a9965.chunk.js"
+  },
+  {
+    "revision": "f8073a64c3f3c91f5709",
+    "url": "/webplayer/static/js/runtime-main.a6eb8836.js"
+  },
+  {
+    "revision": "f97c23852f00b2832c545be6411c5af0",
+    "url": "/webplayer/static/media/album-art-placeholder.f97c2385.png"
+  }
+]);
